a87de4054212e1fc3fea30997d38f28fc7163aff
曾伟明 <851008842@qq.com>
2018-11-02 13:37:44 +0800

    新增流量中间页，router可以传query参数 http://localhost:8080/weixinopen/outserver/html/flow/?cityId=43

---
+++ src/pages/flow/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Cover v-bind:cover-list="coverList" v-bind:city-id="this.$route.query.cityId" />
+  </div>
+</template>
+
+<script>
+import Cover from './components/Cover.vue'
+import data from './config/data.js'
+
+export default {
+  name: 'app',
+  components: {
+    Cover
+  },
+  data () {
+    return {
+      coverList: data.coverList
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  background: #ffffff;
+}
+</style>
